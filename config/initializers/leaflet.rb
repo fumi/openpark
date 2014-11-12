d224aeacd58bc51475b020b29c330ae52e10aede
Fumihiro Kato <fumi@fumi.me>
2014-11-12 21:38:23 +0900

    Added config/initializers/leaflet.rb

---
+++ config/initializers/leaflet.rb
@@ -0,0 +1,4 @@
+Leaflet.tile_layer = "http://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png"
+#Leaflet.tile_layer = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+Leaflet.attribution = '<a href="http://portal.cyberjapan.jp/help/termsofuse.html" target="_blank">国土地理院</a>'
+Leaflet.max_zoom = 18
