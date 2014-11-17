cbf3f6f148bdb2076b1799ef31976a30d8100012
Fumihiro Kato <fumi@fumi.me>
2014-11-17 18:47:58 +0900

    Chagned to use sparql to show park markers

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,26 @@
+# -*- coding: utf-8 -*-
 class HomeController < ApplicationController
   def index
+    client = SPARQL::Client.new("http://localhost:8890/sparql")
+    query = """PREFIX geo: <#{RDF::GEO.to_s}>
+PREFIX rdfs: <#{RDF::RDFS.to_s}>
+PREFIX schema: <#{RDF::SCHEMA.to_s}>
+
+SELECT ?park ?label ?lat ?long
+WHERE {
+  ?park a schema:Park ;
+    rdfs:label ?label ;
+    geo:lat ?lat ;
+    geo:long ?long .
+}
+    """
+    results = client.query(query)
+
+    @markers = []
+    results.each do |solution|
+      @markers << { :latlng => [solution[:lat].to_s.to_f, solution[:long].to_s.to_f],
+        :popup => "<a href=\"#{solution[:park].to_s}\">#{solution[:label].to_s}</a>" }
+#        :popup => "#{solution[:label].to_s}" }
+    end
   end
 end
