0a5fa85bd2b91e06c02b7e9dc5f5225fb4221907
Fumihiro Kato <fumi@fumi.me>
2014-11-17 18:49:29 +0900

    Addes sass comment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,8 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
+  # SASS
   config.sass.preferred_syntax = :scss
   config.sass.cache = false
+
 end
