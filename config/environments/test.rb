f2b4418eb09cec85d332d1baa5a54d2274d901fa
Fumihiro Kato <fumi@fumi.me>
2014-12-04 19:02:21 +0900

    Added config.sparql_endpoint

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -36,4 +36,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.sparql_endpoint = "http://localhost:8890/sparql"
+
 end
