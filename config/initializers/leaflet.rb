9428a02fceafa0dec598f4a6d03e4ee125e904f2
Fumihiro Kato <fumi@fumi.me>
2014-12-05 15:19:27 +0900

    Fixed to utf-8

--- config/initializers/leaflet.rb
+++ config/initializers/leaflet.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 Leaflet.tile_layer = "http://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png"
 #Leaflet.tile_layer = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png'
 Leaflet.attribution = '<a href="http://portal.cyberjapan.jp/help/termsofuse.html" target="_blank">国土地理院</a>'
