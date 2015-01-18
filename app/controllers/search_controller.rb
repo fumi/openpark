f4d26ea075d9abd745d23724c06b8c89c0f354fe
Fumihiro Kato <fumi@fumi.me>
2015-01-18 18:56:48 +0900

    Support age limit search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -10,76 +10,64 @@ PREFIX park: <#{PARK.to_s}>
 PREFIX dcterms: <#{RDF::DC.to_s}>
 """
 
-    if params[:equipmentType] and !params[:equipmentType].empty?
-      equipment_type = params[:equipmentType]
+    if (params[:equipmenType] and !params[:equipmentType].empty?) ||
+       (params[:age] and !params[:age].empty?)
+
+      equipment_type = params[:equipmentType] || "全て"
+      age = params[:age].to_s || "3"
 
       query = prefixes +
 """
-SELECT DISTINCT ?uri ?label ?postalCode ?address ?lat ?long
+SELECT DISTINCT ?park ?parkLabel ?postalCode ?address ?lat ?long ?equipment ?equipmentLabel ?age_max ?age_min
 WHERE {
-  ?uri a park:公園 ;
-    rdfs:label ?label ;
-    """
+  ?park a park:公園 ;
+    rdfs:label ?parkLabel ;
+    ic:地点_設備 ?equipment ;
+    ic:地点_場所/ic:場所_住所 [
+      ic:住所_郵便番号 ?postalCode ;
+      ic:住所_表記 ?address 
+    ] ;
+    geo:lat ?lat ;
+    geo:long ?long .
+  ?equipment rdfs:label ?equipmentLabel .
+"""
       unless  equipment_type.match("全て")
-        query << "    ic:地点_設備/dcterms:subject park:#{equipment_type} ;"
+        query << "  ?equipment dcterms:subject park:#{equipment_type} ."
       end
-      query << """
-    ic:地点_場所/ic:場所_住所 [
-      ic:住所_郵便番号 ?postalCode ;
-      ic:住所_表記 ?address 
-    ] ;
-    geo:lat ?lat ;
-    geo:long ?long .
-}
-ORDER BY ?label
-"""
-      @parks = client.query(query)
-    elsif params[:age] and !params[:age].empty?
-      # 結果は年齢制限が明示的にあるものだけにする
-      age = params[:age].to_s
-      query = prefixes +
-"""
-SELECT DISTINCT ?uri ?label ?postalCode ?address ?lat ?long
-WHERE {
-  ?uri a park:公園 ;
-    rdfs:label ?label ;
-    ic:地点_場所/ic:場所_住所 [
-      ic:住所_郵便番号 ?postalCode ;
-      ic:住所_表記 ?address 
-    ] ;
-    ic:地点_設備/dcterms:subject ?equipment ;
-    geo:lat ?lat ;
-    geo:long ?long .
+
+  query << """
   OPTIONAL {
-    ?equipment park:年齢上限 ?upper_age_limit .
-    FILTER (?upper_age_limit > #{age})
+    ?equipment park:年齢上限 ?age_max .
+  FILTER (?age_max > #{age})
   }
   OPTIONAL {
-    ?equipment park:年齢下限 ?lower_age_limit .
-    FILTER (?lower_age_limit < #{age})
+    ?equipment park:年齢下限 ?age_min .
+    FILTER (?age_min < #{age})
   }
 }
-ORDER BY ?label
+ORDER BY ?parkLabel
 """
+      puts query
       @parks = client.query(query)
+
     elsif params[:text] and !params[:text].empty?
       text = params[:text]
 
       query = prefixes +
 """
-SELECT DISTINCT ?uri ?label ?postalCode ?address ?lat ?long
+SELECT DISTINCT ?park ?parkLabel ?postalCode ?address ?lat ?long
 WHERE {
-  ?uri a park:公園 ;
-    rdfs:label ?label ;
+  ?park a park:公園 ;
+    rdfs:label ?parkLabel ;
     ic:地点_場所/ic:場所_住所 [
       ic:住所_郵便番号 ?postalCode ;
       ic:住所_表記 ?address 
     ] ;
     geo:lat ?lat ;
     geo:long ?long .
-  FILTER regex(?label, '#{text}')
+  FILTER regex(?parkLabel, '#{text}')
 }
-ORDER BY ?label
+ORDER BY ?parkLabel
 """
 
       @parks = client.query(query)
