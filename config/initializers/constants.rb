f308aa753d98b3f2b34ea1c8f12ee7e42eacf585
Fumihiro Kato <fumi@fumi.me>
2016-05-10 19:52:56 +0900

    Changed to prefixes

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,17 +1,17 @@
 # Constants variables
 
-PARK_RESOURCE = RDF::Vocabulary.new("http://yokohama.openpark.jp/parks/")
-EQUIPMENT_RESOURCE = RDF::Vocabulary.new("http://yokohama.openpark.jp/equipment/")
-ORGANIZATION_RESOURCE = RDF::Vocabulary.new("http://yokohama.openpark.jp/organizations/")
-IC = RDF::Vocabulary.new("http://imi.ipa.go.jp/ns/core/210#")
-PARK = RDF::Vocabulary.new("http://yokohama.openpark.jp/ns/park#")
+PARK_RESOURCE = RDF::Vocabulary.new("http://openpark.jp/parks/")
+EQUIPMENT_RESOURCE = RDF::Vocabulary.new("http://openpark.jp/equipment/")
+ORGANIZATION_RESOURCE = RDF::Vocabulary.new("http://openpark.jp/organizations/")
+IC = RDF::Vocabulary.new("http://imi.ipa.go.jp/ns/core/rdf#")
+PARK = RDF::Vocabulary.new("http://openpark.jp/ns/park#")
 
 PREFIXES = {
   :rdf => RDF.to_s,
   :rdfs => RDF::RDFS.to_s,
-  :schema => RDF::SCHEMA.to_s,
-  :geo => RDF::GEO.to_s,
-  :dcterms => RDF::DC.to_s,
+  :schema => RDF::Vocab::SCHEMA.to_s,
+  :geo => RDF::Vocab::GEO.to_s,
+  :dcterms => RDF::Vocab::DC.to_s,
   :xsd => RDF::XSD.to_s,
   :park_resource => PARK_RESOURCE.to_s,
   :equipment_resource => EQUIPMENT_RESOURCE.to_s,
