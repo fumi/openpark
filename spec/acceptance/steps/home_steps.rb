ba8dfde0616eb2a042a9123a4af8d19bb4f4f7d3
Fumihiro Kato <fumi@fumi.me>
2016-05-19 18:30:46 +0900

    Renamed step.rb to steps.rb

--- spec/acceptance/steps/home_steps.rb
+++ spec/acceptance/steps/home_steps.rb
@@ -1,5 +1,9 @@
 # encoding: utf-8
 module HomepageSteps
+  step %(:pageページにアクセスする) do |page|
+    visit "#{page}"
+  end
+
   step "画面に地図が表示されていること" do
     except(page).to have_content("#map")
   end
