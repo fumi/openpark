927e13f89ef6cac01f7bc46ebd3ce0b554cd396a
Fumihiro Kato <fumi@fumi.me>
2016-05-23 22:33:40 +0900

    Fixed search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,36 +1,38 @@
 # -*- coding: utf-8 -*-
 class SearchController < ApplicationController
   def index
+    params.permit(:equipment_type, :age, :text)
+
     client = SPARQL::Client.new(OpenPark::Application.config.sparql_endpoint)
     prefixes = """PREFIX ic: <#{IC.to_s}>
 PREFIX park: <#{PARK.to_s}>
 """
 
-    if (params[:equipmenType] and !params[:equipmentType].empty?) ||
+    if (params[:equipmen_type] and !params[:equipment_type].empty?) ||
        (params[:age] and !params[:age].empty?)
 
-      equipment_type = params[:equipmentType] || "全て"
+      equipment_type = params[:equipment_type] || "全て"
       age = params[:age].to_s || "3"
 
       query = prefixes +
 """
-SELECT DISTINCT ?park ?parkLabel ?postalCode ?address ?lat ?long ?equipment ?equipmentLabel ?age_max ?age_min
+SELECT DISTINCT ?park ?park_label ?postal_code ?address ?lat ?long ?equipment ?equipment_label ?age_max ?age_min
 WHERE {
-  ?park a ic:施設 ;
-    ic:名称/ic:表記 ?parkLabel ;
+  ?park a ic:施設型 ;
+    ic:名称/ic:表記 ?park_label ;
     ic:設備 ?equipment ;
     ic:住所 [
-      ic:郵便番号 ?postalCode ;
+      ic:郵便番号 ?postal_code ;
       ic:表記 ?address
     ] ;
     ic:地理識別子 [
       ic:経度 ?long ;
       ic:緯度 ?lat
     ] .
-  ?equipment ic:名称/ic:表記 ?equipmentLabel .
+  ?equipment ic:名称/ic:表記 ?equipment_label .
 """
       unless  equipment_type.match("全て")
-        query << "  ?equipment ic:種別 \"#{equipment_type}\"@ja ."
+        query << "  ?equipment park:種別 \"#{equipment_type}\"@ja ."
       end
 
   query << """
@@ -43,7 +45,7 @@ WHERE {
     FILTER (?age_min <= #{age})
   }
 }
-ORDER BY ?parkLabel
+ORDER BY ?park_label
 """
       @parks = client.query(query)
 
@@ -52,23 +54,22 @@ ORDER BY ?parkLabel
 
       query = prefixes +
 """
-SELECT DISTINCT ?park ?parkLabel ?postalCode ?address ?lat ?long
+SELECT DISTINCT ?park ?park_label ?postal_code ?address ?lat ?long
 WHERE {
   ?park a ic:施設型 ;
-    ic:名称/ic:表記 ?parkLabel ;
+    ic:名称/ic:表記 ?park_label ;
     ic:住所 [
-      ic:郵便番号 ?postalCode ;
+      ic:郵便番号 ?postal_code ;
       ic:表記 ?address
     ] ;
     ic:地理識別子 [
       ic:経度 ?long ;
       ic:緯度 ?lat
     ] .
-    FILTER regex(?parkLabel, '#{text}')
+    FILTER regex(?park_label, '#{text}')
 }
-ORDER BY ?parkLabel
+ORDER BY ?park_label
 """
-
       @parks = client.query(query)
     end
   end
