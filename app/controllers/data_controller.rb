080ef199454b96f83c134a6d381d0e1cfa211ad5
Fumihiro Kato <fumi@fumi.me>
2017-06-19 19:05:14 +0900

    Testing JSON-LD context

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -5,29 +5,33 @@ class DataController < ApplicationController
 PREFIX ic: <http://imi.go.jp/ns/core/rdf#>
 PREFIX park: <http://openpark.jp/ns/park#>
 CONSTRUCT {
-  <%s> a park:公園型 ;
+  ?root a park:公園型 ;
     ic:名称 [ a ic:名称型; ic:表記 ?label ] ;
     ic:住所 [ a ic:住所型; ic:表記 ?address ] ;
     ic:郵便番号 ?postalcode ;
     ic:地理座標 [ a ic:座標型; ic:経度 ?long ; ic:緯度 ?lat ] ;
-    ic:説明 ?description .
+    ic:説明 ?description ;
+    ic:設備 ?equipment ;
+    park:面積 [ a ic:面積型; ic:数値 ?area ]
+  .
 }
 WHERE {
-  <%s> a park:公園型 ;
+  ?root a park:公園型 ;
     ic:名称/ic:表記 ?label ;
     ic:住所/ic:表記 ?address .
+  OPTIONAL { ?root ic:郵便番号 ?postalcode . }
   OPTIONAL {
-    <http://openpark.jp/parks/14108/P7005> ic:郵便番号 ?postalcode .
-  }
-  OPTIONAL {
-    <http://openpark.jp/parks/14108/P7005> ic:地理座標 [
+    ?root ic:地理座標 [
       ic:経度 ?long ;
       ic:緯度 ?lat
     ] .
   }
   OPTIONAL {
-    <http://openpark.jp/parks/14108/P7005> ic:説明 ?description .
+    ?root ic:設備 ?equipment .
   }
+  OPTIONAL { ?root ic:説明 ?description . }
+  OPTIONAL { ?root park:面積/ic:数値 ?area . }
+  BIND (<%s> AS ?root)
 }
 LIMIT 1
 EOQ
@@ -51,31 +55,38 @@ EOQ
   protected
 
   def dump_jsonld(format, uri)
-    client = SPARQL::Client.new(OpenPark::Application.config.sparql_endpoint)
-    query = @query_template % [uri, uri]
-    sparql_result = client.query(query)
+#    "http://openpark.jp/contexts/park"
     frame = JSON.parse %({
   "@context": [
-    "http://imi.go.jp/ns/core/context.jsonld",
-    "http://openpark.jp/context/park"
+    "http://openpark.jp/contexts/park"
   ],
-  "@type": "公園型",
+  "@type": "公園型"
   "住所": {
     "@type": "住所型"
   },
   "地理座標": {
     "@type": "座標型"
+  },
+  "面積": {
+    "@type": "面積型"
   }
 })
+#  "@type": "公園型"
+    client = SPARQL::Client.new(OpenPark::Application.config.sparql_endpoint)
+    query = @query_template % uri
+    puts query
+    sparql_result = client.query(query)
     input = JSON.parse(sparql_result.dump(:jsonld))
+    puts input
     test = JSON::LD::API.frame(input, frame)
+    puts test
     JSON.pretty_generate(test)
     #input = sparql_result.dump(format, :standard_prefixes => true, :context => frame)
   end
 
   def dump_rdf(format, uri)
     client = SPARQL::Client.new(OpenPark::Application.config.sparql_endpoint)
-    query = @query_template % [uri, uri]
+    query = @query_template % uri
     sparql_result = client.query(query)
     input = sparql_result.dump(format, :standard_prefixes => true, :prefixes => PREFIXES)
   end
