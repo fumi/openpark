80801aa2a1629869833be06cad13f4ac7d6e0903
Fumihiro Kato <fumi@fumi.me>
2017-06-19 18:14:43 +0900

    Fixed hostname

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.sparql_endpoint = "http://localhost:8890/sparql"
-  config.hostname = "yokohama.openpark.jp"
+  config.hostname = "openpark.jp"
 
 end
