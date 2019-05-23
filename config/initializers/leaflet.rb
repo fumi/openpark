d9ac4b382cc3170fcf5164d23a97ea14b47c8869
Fumihiro Kato <fumi@fumi.me>
2019-05-23 13:56:42 +0900

    Updated http to https

--- config/initializers/leaflet.rb
+++ config/initializers/leaflet.rb
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-Leaflet.tile_layer = "http://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png"
+Leaflet.tile_layer = "https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png"
 #Leaflet.tile_layer = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-Leaflet.attribution = '<a href="http://portal.cyberjapan.jp/help/termsofuse.html" target="_blank">国土地理院</a>'
+Leaflet.attribution = '<a href="https://portal.cyberjapan.jp/help/termsofuse.html" target="_blank">国土地理院</a>'
 Leaflet.max_zoom = 18
